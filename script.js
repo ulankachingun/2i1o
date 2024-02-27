605964fb3bc7c61abff8f49c870bd4a47cfe4cdf
ulankachingun <145523623+ulankachingun@users.noreply.github.com>
2024-02-27 15:06:43 +0000

    done

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-const names=[];
-//const nam=[];
-
-function FistFunction(){
-    let box='';
-    
-    for(let i=0; i < names.length; i++){
-        box+='<p class="red">' +'Bat:' + names[i]+"<br>";
-        console.log(i,": ",box);
-    }
-    document.getElementById('list').innerHTML=box;
+function displayMessage(name) {
+    const inn = document.getElementById('inn').value;
+    const list = document.getElementById('chat');
+    const newMessage = document.createElement('li');
+    newMessage.textContent = `${name}: ${inn}`;
+    list.appendChild(newMessage);
+    document.getElementById('inn').value = '';
 }
-function SecondFunction(){
-    let box2='';
-    
-    for(let i=0; i < names.length; i++){
-        box2+='<p class="red">' +'Dorj:' + names[i]+"<br>";
-        console.log(i,": ",box2);
-    }
-    document.getElementById('list').innerHTML=box2;
-}
-
-function S1(){
-let newName = document.getElementById('bat').value;
-names.push(newName);
-// console.log(newName)
-FistFunction()
-}
-
-function S2(){
-    let dorj = document.getElementById('dorj').value;
-    names.push(dorj);
-    // console.log(dorj)
-    SecondFunction()
-    }
-    
